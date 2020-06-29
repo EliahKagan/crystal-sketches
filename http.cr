38f5b62b0bd295c0152b8fa202ffa76275a81ae6
Eliah Kagan <degeneracypressure@gmail.com>
2020-06-28 20:07:04 -0400

    simple HTTP server example

--- http.cr
+++ http.cr
@@ -1,5 +1,10 @@
 require "http/server"
 
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/plain"
+  context.response.print "Hello, world! The time is #{Time.local}!"
+end
 
-# server = HTTP::server.new do |context|
-#     context.response.content_type = "text/plain"
+address = server.bind_tcp 8080
+puts "Listening on http://#{address}"
+server.listen
