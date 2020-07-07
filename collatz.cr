121917c7f25209f70b9613218e63003255639203
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-07 00:49:30 -0400

    better variable names

--- collatz.cr
+++ collatz.cr
@@ -1,16 +1,16 @@
-def collatz(n)
-  yield n
-  while n != 1
-    n = (n.even? ? n // 2 : n * 3 + 1)
-    yield n
+def collatz(seed)
+  yield seed
+  while seed != 1
+    seed = (seed.even? ? seed // 2 : seed * 3 + 1)
+    yield seed
   end
 end
 
 
 extra_newline = false
 
-ARGV.map(&.to_i).each do |arg|
+ARGV.map(&.to_i).each do |seed|
   puts if extra_newline
-  collatz(arg) { |n| puts n }
+  collatz(seed) { |value| puts value }
   extra_newline = true
 end
