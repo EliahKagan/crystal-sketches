1d8644f1cb4e12c2bff9b4fd351a633094887f09
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-09 22:25:11 -0400

    demonstrate same difference in Ruby

--- overload.cr
+++ overload.cr
@@ -1,3 +1,11 @@
+# As stated in:
+#
+# https://crystal-lang.org/reference/syntax_and_semantics/blocks_and_procs.html
+#
+# "The difference between using `do ... end` and `{ ... }` is that `do ... end`
+#  binds to the left-most call, while `{ ... }` binds to the right-most call"
+
+
 # This f accepts: one argument + a block.
 def f(x)
   puts "Called f(#{x}) with a block."
---
+++ overload.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Unlike Crystal, Ruby doesn't support overloading. But it does support
+# optional blocks via block_given?. This program tests if the major non-obvious
+# syntactic difference between do ... end and { ... } style blocks in Crystal
+# also appears in Ruby. See overload.cr.
+
+$VERBOSE = 1
+
+
+def f(x)
+  if block_given?
+    puts "Called f(#{x}) with a block."
+    yield x
+  else
+    puts "Called f(#{x}), no block."
+  end
+end
+
+def g
+  if block_given?
+    puts 'Called g() with a block.'
+    yield
+  else
+    puts 'Called g(), no block.'
+  end
+end
+
+if __FILE__ == $0
+  f g do
+    puts '...in a do-end block.'
+  end
+
+  puts
+
+  f g { puts '...in a braced block.' }
+end
