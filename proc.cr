b779dc9616579079d1eed021a98957cd016bf12c
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-12 03:40:44 -0400

    less inelegant

--- proc.cr
+++ proc.cr
@@ -1,11 +1,5 @@
-def push_block(array, &block)
-  array.push(block)
-end
-
-acts = [] of Proc(Nil)
-
-%w[Alice Bob Cassidy].each do |name|
-  push_block(acts) { puts "Hello, #{name}!" }
+acts = %w[Alice Bob Cassidy].map do |name|
+  ->() { puts "Hello, #{name}!" }
 end
 
 acts.each(&.call)
