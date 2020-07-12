8d323ac720230293e28ba271f42933329c17c707
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-12 03:25:46 -0400

    capturing loop variable

--- proc.cr
+++ proc.cr
@@ -1,3 +1,11 @@
 def push_block(array, &block)
   array.push(block)
 end
+
+acts = [] of Proc(Nil)
+
+%w[Alice Bob Cassidy].each do |name|
+  push_block(acts) { puts "Hello, #{name}!" }
+end
+
+acts.each(&.call)
