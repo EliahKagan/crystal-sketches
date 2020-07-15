881a486d326f0010dec530fab5003ce569335ef3
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-15 13:32:49 -0400

    hmm, this time making a copy seems to solve it

---
+++ niltrace4.cr
@@ -0,0 +1,19 @@
+def i32_or_default(text : String, default : Int32)
+  value = text.to_i?
+  if value
+    copy : Int32 = value
+    -> { copy }
+  else
+    -> { default }
+  end
+end
+
+f = i32_or_default("100", 42)
+g = i32_or_default("10z", 42)
+x = f.call
+y = g.call
+
+puts "f   #{typeof(f)}   #{f.class}"
+puts "g   #{typeof(g)}   #{g.class}"
+puts "x   #{typeof(x)}   #{x.class}   #{x}"
+puts "y   #{typeof(y)}   #{y.class}   #{y}"
