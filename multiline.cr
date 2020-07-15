69eb353e928740998e7bf1b369985dbb62d879f7
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-14 21:50:01 -0400

    inconsistent .tap design

--- multiline.cr
+++ multiline.cr
@@ -3,6 +3,7 @@ lines = ["123 -> x", "456 -> y", "", " x AND y -> d  ", "  \t ", "\ty RSHIFT\t2 
 lines.map(&.strip)
      .reject(&.empty?)
      .map(&.split(/\s+->\s+/))
+     .tap { |thing| pp thing }
      .each { |thing| pp thing }
     #.tap { |tokens| raise "malformed expression" unless tokens.size == 2 }
     #.each { |tokens| puts "(#{tokens[0]}, #{tokens[1]})" }
