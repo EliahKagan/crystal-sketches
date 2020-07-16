92bcfab3ed5e6f914ba0e9abed723c26da7a671a
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-16 07:29:35 -0400

    can't restrict to just some procs this way

---
+++ procverload.cr
@@ -0,0 +1,10 @@
+def apply(arg : T, &block : Proc(T, Nil)) forall T
+  yield arg
+end
+
+def apply(&block : Proc(T, Nil)) forall T
+  yield 42
+end
+
+apply(17) { |x| puts x * x }
+apply { |x| puts x * x }
