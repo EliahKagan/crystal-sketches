b426925a3236536bd210e9745a44f770f9024b2b
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-26 12:07:49 -0400

    heredocs

---
+++ heredoc.cr
@@ -0,0 +1,11 @@
+<<-'EOF'.split('\n').zip(<<-'EOF'.split('\n')) { |a, b| puts "#{a} #{b}" }
+'Twas brillig, and
+Did gyre and
+All mimsy were
+And the
+EOF
+the slithy toves
+gimble in the wabe
+the borogroves,
+mome raths outgrabe.
+EOF
