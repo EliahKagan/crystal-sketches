699d371d6556859ab93bfef4ab9a1add11b730c0
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-26 22:16:15 -0400

    various delimiter indentations

--- leading.cr
+++ leading.cr
@@ -1,13 +1,27 @@
-count1 = <<-STOP.count &.whitespace?
+count0 = <<-STOP.count &.whitespace?
   one two three
   four five six
   seven eight nine
 STOP
-puts "There are #{count1} whitespace characters with an unindented delimiter."
+puts "There are #{count0} whitespace characters with an unindented delimiter."
+
+count1 = <<-STOP.count &.whitespace?
+  one two three
+  four five six
+  seven eight nine
+ STOP
+puts "There are #{count1} whitespace characters with a slighly indented delimiter."
 
 count2 = <<-STOP.count &.whitespace?
   one two three
   four five six
   seven eight nine
   STOP
-puts "There are #{count2} whitespace characters with an indented delimiter."
+puts "There are #{count2} whitespace characters with a more indented delimiter."
+
+count3 = <<-STOP.count &.whitespace?
+  one two three
+  four five six
+  seven eight nine
+    STOP
+puts "There are #{count3} whitespace characters with an overly indented delimiter."
