00083aed5a5b6ccf173fd72219730882a8b5d53d
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-26 22:16:39 -0400

    removed code that broke compile

--- leading.cr
+++ leading.cr
@@ -18,10 +18,3 @@ count2 = <<-STOP.count &.whitespace?
   seven eight nine
   STOP
 puts "There are #{count2} whitespace characters with a more indented delimiter."
-
-count3 = <<-STOP.count &.whitespace?
-  one two three
-  four five six
-  seven eight nine
-    STOP
-puts "There are #{count3} whitespace characters with an overly indented delimiter."
