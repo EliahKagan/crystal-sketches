1b5122f64cff08759b5f7190fb35dd702a3762cb
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-29 01:46:16 -0400

    fixed demonstration of a capture keeping a type general

--- gets-thunk.cr
+++ gets-thunk.cr
@@ -1,11 +1,34 @@
+# Just reports type and value.
+def no_capture(n : Int32?)
+  if n.nil?
+    puts "Got nil."
+  else
+    puts "Got #{n}, static type #{typeof(n)}."
+  end
+
+  ->{ puts "This one never captures." }
+end
+
 # Creates a proc that, when called, prints a value and its type.
-def f(n : Int32?)
+def capture_param(n : Int32?)
   if n.nil?
     puts "Got nil."
     ->{ puts "No capture." }
   else
-    puts "Got #{typeof(n)}."
-    ->{ puts "Captured #{typeof(n)}." }
+    puts "Got #{n}, static type #{typeof(n)}."
+    ->{ puts "Captured #{n}, static type #{typeof(n)}." }
+  end
+end
+
+# Same, but manages more specific typing.
+def capture_copy(n : Int32?)
+  if n.nil?
+    puts "Got nil."
+    ->{ puts "No capture." }
+  else
+    puts "Got #{n}, static type #{typeof(n)}."
+    copy = n
+    ->{ puts "Captured #{copy}, static type #{typeof(copy)}." }
   end
 end
 
@@ -15,5 +38,14 @@ if ARGV.empty?
 end
 
 n = ARGV[0].to_i?
-g = f(n)
+
+f = no_capture(n)
+f.call
+
+puts
+g = capture_param(n)
 g.call
+
+puts
+h = capture_copy(n)
+h.call
