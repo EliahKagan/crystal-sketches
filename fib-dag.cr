bbe57b09ada22cd770e96c211c9d20f3304623ab
Eliah Kagan <degeneracypressure@gmail.com>
2020-07-31 14:57:01 -0400

    fixed horrendous wrongness

--- fib-dag.cr
+++ fib-dag.cr
@@ -1,12 +1,12 @@
-INDENT = "    "
+INDENT = "   "
 
 raise "wrong number of arguments" if ARGV.size != 1
 n = ARGV[0].to_i
 
 puts "digraph G {"
 
-0.upto(n - 2) do |src|
-  (src + 1).upto(src + 2) { |dest| puts "#{INDENT} #{src} -> #{dest}" }
+n.downto(2) do |src|
+  {src - 2, src - 1}.each { |dest| puts "#{INDENT} #{src} -> #{dest}" }
 end
 
 puts "}"
