1dabb27f93102763f943c11234093c1d6ea397e4
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-03 12:22:52 -0400

    print the types

--- arg2.cr
+++ arg2.cr
@@ -4,7 +4,11 @@ def die(message)
 end
 
 arg = ARGV[0]?
+puts "arg : #{typeof(arg)}"
 die "too few arguments" if arg.nil?
+puts "arg : #{typeof(arg)}"
 n = arg.to_i?
+puts "n : #{typeof(n)}"
 die %[can't square #{ARGV[0]}] if n.nil?
+puts "n : #{typeof(n)}"
 puts "#{n} squared is #{n**2}."
