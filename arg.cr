f55b4368f0a82282bbe7b7ed51ea4d5bfc4a53be
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-03 11:30:01 -0400

    catch both exceptions

--- arg.cr
+++ arg.cr
@@ -1,7 +1,13 @@
+def die(message)
+  STDERR.puts "#{PROGRAM_NAME}: error: #{message}"
+  exit 1
+end
+
 begin
   n = ARGV[0].to_i
   puts "#{n} squared is #{n**2}."
+rescue IndexError
+  die "too few arguments"
 rescue ArgumentError
-  STDERR.puts %[#{PROGRAM_NAME}: error: can't square "#{ARGV[0]}"]
-  exit 1
+  die %[can't square "#{ARGV[0]}"]
 end
