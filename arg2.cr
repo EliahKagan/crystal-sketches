acaddafbcf5c1fafa3eb7bf14835756ab138ab90
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-03 12:11:32 -0400

    handle both runtime errors (with []? and to_i?)

--- arg2.cr
+++ arg2.cr
@@ -1,8 +1,10 @@
-n = ARGV[0].to_i?
-
-if n.nil?
-  STDERR.puts %[#{PROGRAM_NAME}: error: can't square #{ARGV[0]}]
+def die(message)
+  STDERR.puts "#{PROGRAM_NAME}: error: #{message}"
   exit 1
 end
 
+arg = ARGV[0]?
+die "too few arguments" if arg.nil?
+n = arg.to_i?
+die %[can't square #{ARGV[0]}] if n.nil?
 puts "#{n} squared is #{n**2}."
