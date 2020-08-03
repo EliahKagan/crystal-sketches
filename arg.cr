1e4c2c19fa9b4eed3634906909d3fc84bf86e3f3
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-03 11:16:49 -0400

    catch the exception

--- arg.cr
+++ arg.cr
@@ -1,2 +1,6 @@
-n = ARGV[0].to_i
-puts "#{n} squared is #{n**2}."
+begin
+  n = ARGV[0].to_i
+  puts "#{n} squared is #{n**2}."
+rescue ArgumentError
+  STDERR.puts %[#{PROGRAM_NAME}: error: can't square "#{ARGV[0]}"]
+end
