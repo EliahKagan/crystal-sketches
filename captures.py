ab8759ab8853532be2dc6a81c709707d142f0797
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-09 00:10:32 -0400

    made it local

--- captures.py
+++ captures.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-actions = []
-for i in range(1, 6):
-    actions.append(lambda: print(f'{i} squared is {i**2}.'))
+def run():
+    actions = []
+    for i in range(1, 6):
+        actions.append(lambda: print(f'{i} squared is {i**2}.'))
 
-for f in actions:
-    f()
+    for f in actions:
+        f()
+
+if __name__ == '__main__':
+    run()
