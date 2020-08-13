e1122b24ba47197e75720c87e7cde1e01c216e0f
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-13 17:33:52 -0400

    proc-like objects can't always be used like procs

---
+++ procoid.cr
@@ -0,0 +1,18 @@
+# Does &obj work when obj is not a proc but has a call method?
+
+words = %w[foo bar baz quux foobar]
+
+# With a proc:
+tc1 = ->(word : String) { word[0].upcase + word[1..] }
+pp words.map(&tc1)
+
+# A "procoid" class whose instances behave like tc1 above.
+class TitleCaser
+  def call(word)
+    word[0].upcase + word[1..]
+  end
+end
+
+# With a procoid:
+tc2 = TitleCaser.new
+pp words.map(&tc2) # Compile error. & syntax doesn't support this.
