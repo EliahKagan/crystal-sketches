cf29d38697c70aaca14433e40057af218afb6c64
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-15 18:26:20 -0400

    can have unexpected results

--- tuples.cr
+++ tuples.cr
@@ -1,5 +1,13 @@
-def f(t : Tuple(T, T)) forall T
-  puts "Called f with #{t}. typeof(t)==#{typeof(t)}. t.class=#{t.class}."
+# def f(t : Tuple(T, T)) forall T
+#   puts "Called f with #{t}. typeof(t)==#{typeof(t)}. t.class=#{t.class}."
+# end
+
+def f(t : Tuple(Int32, Int32))
+  puts "Pair of Int32."
+end
+
+def f(t : Tuple(String, String))
+  puts "Pair of String."
 end
 
 u = {10, 20}
@@ -9,4 +17,4 @@ f(u)
 f(v)
 
 w = rand < 0.5 ? u : v
-f(w)
+f(w) # compile error!
