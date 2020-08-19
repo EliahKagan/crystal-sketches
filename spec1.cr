9a34ee1198457b81a3f39897bfd24e0ddc78cbdd
Eliah Kagan <degeneracypressure@gmail.com>
2020-08-18 23:27:07 -0400

    standalone spec file

---
+++ spec1.cr
@@ -0,0 +1,12 @@
+require "spec"
+
+describe Array do
+  describe "#[]" do
+    it "indexes backward from the end with negative subscripts" do
+      a = %w[Alice Bob Cassidy]
+      a[-1].should eq "Cassidy"
+      a[-2].should eq "Bob"
+      a[-3].should eq "Alice"
+    end
+  end
+end
