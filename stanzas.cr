575e2b87a76c0190371d9a39b9559ab57b8a2a93
Eliah Kagan <degeneracypressure@gmail.com>
2020-12-16 09:15:11 -0500

    inlined take_until doesn't work for the expected reason

--- stanzas.cr
+++ stanzas.cr
@@ -1,5 +1,11 @@
+module Iterator(T)
+  def take_until(&block)
+    take_while { |item| !yield(item) }
+  end
+end
+
 loop do
-  lines = ARGF.each_line.map(&.rstrip).take_while { |line| !line.empty? }.to_a
+  lines = ARGF.each_line.map(&.rstrip).take_until(&.empty?).to_a
   break if lines.empty?
   lines.each { |line| puts line }
 end
